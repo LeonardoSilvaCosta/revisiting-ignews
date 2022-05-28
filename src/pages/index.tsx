86cd842e4b4cbfbac785aa9eff8031212fff0142
leonardocosta <sir.costa@yahoo.com.br>
2022-05-28 19:10:28 -0300

    Adicionando Image do next e dependências de useEffect.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import { GetStaticProps } from 'next';
+import Image from 'next/image'
 import Head from "next/head";
-import { useEffect } from 'react';
 import { SubscribeButton } from "../components/SubscribeButton";
 import { stripe } from '../services/stripe';
 
@@ -34,7 +34,7 @@ export default function Home({ product }: HomeProps) {
           <SubscribeButton />
         </section>
 
-        <img src="/images/avatar.svg" alt="Girl coding" />
+        <Image src="/images/avatar.svg" alt="Girl coding" />
       </main>
     </>
   );
